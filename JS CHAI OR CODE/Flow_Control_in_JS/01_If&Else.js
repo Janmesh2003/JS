abe37afbbc79dfe74f39f3377fb4612f616d403a
Janmesh2003 <someshthakur229@gmail.com>
2024-06-05 18:13:15 +0530

    Add files via upload

---
+++ JS CHAI OR CODE/Flow_Control_in_JS/01_If&Else.js
@@ -0,0 +1,69 @@
+/********************************   IF AND ELSE    ****************************/ 
+
+// if( true  )
+// {
+//     console.log(`"YEh Function tho excute hi hona tha kyuki If ke Ander maine true diya condition ki place pr"`);
+// }
+
+// if ( 3 == 2)
+// {
+//     console.log(`"Yeh print nhi hoga"`)
+// }
+
+/******************************************************************/
+
+// let Score = 200
+// if(Score > 100)
+// {
+//     let power = "Run"
+//     console.log(`Power is `, Score);
+// }
+// console.log(power); /* yeh power data type ko hum acces nhi kar sakte kyuki yeh if condition
+//  ke ander hai mtlb "local" Object hai */
+
+/******************************************************************/
+
+// let balance = 1000
+
+// if (balance < 500)
+// {
+
+//     console.log(`Balance is less than 500`);
+
+// }
+
+// else if (balance < 750)
+// {
+
+//     console.log(`balance is less than 750`);
+
+// }
+
+// else if (balance < 950)
+// {
+
+//     console.log(`balance is less than 950 `);
+
+// }
+// else 
+// {
+//     console.log(`Given balance is greater than 950 `);
+// }
+
+/*********************************************************************/
+
+// let UserLoggedIn = true;
+// let UserDebitCart = true;
+// let UserloginFromGoogle = false;
+// let UserLoginFromBank = true;
+
+// if (UserLoggedIn && UserDebitCart && UserloginFromGoogle)
+//     {
+//         console.log(`verified user`);
+//     }
+// if (UserLoginFromBank || UserloginFromGoogle && UserDebitCart && UserLoggedIn)
+//     {
+//         console.log(`verified user`);
+//     }
+
+/*************************************************************************/
