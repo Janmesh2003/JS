b417d951b1d4abdcefae4a7894346e0cae17c3b0
Janmesh2003 <someshthakur229@gmail.com>
2024-06-05 18:54:38 +0530

    Add files via upload

---
+++ JS CHAI OR CODE/Flow_Control_in_JS/TruthlyAndFalsy.js
@@ -0,0 +1,59 @@
+// const Detect = "janmesh"
+
+// if(Detect)
+// {
+
+//     console.log("Detect ho gya hai ");
+
+// }
+// else
+// {
+//     console.log("not Detect");
+// }
+
+/* Falsy  */
+
+// " false, 0, -0, bigInt 0n, "", Null, undefined, NaN "  /* Is Sabka O/p Else wale part mai jayega  */
+
+
+/* Truthly */
+
+// " "0", 'false', " ", [], {}, function(){} "  /* Yeh Sabka O/p If part mai hoga */
+
+
+// const Arr = []
+
+// if(Arr.length === 0) /* yeh tarika hai Array Empty nahi hai Pata karne ka */
+// {
+//     console.log("Array is Empty");
+// }
+// else
+// {
+//     console.log("Array is not empty");
+// }
+
+/**********************************************************************************/
+
+
+// let ChechObject = {}
+
+// if(Object.keys(ChechObject).length === 0 ) /* Yeh Object Check kiya hai ki object Empty hai nhi */
+// {
+
+//     console.log("Object is Empty");
+
+// }
+// else
+// {
+
+//     console.log("object is not empty ");
+
+// }
+
+/*                   Interview purpose                          */
+
+console.log( false == 0 );      /* Iska O/p => "true " Ata hai */
+console.log( 0 == "" );         /* Same true O/p  */
+console.log( false == "" ) ;    /* o/P => true  */
+console.log( 0 == '');          /* o/p => true */
+console.log(false == '');       /* O/p => true */
