660eae48e0096bfd00344bfa3aec0674fec94d74
Janmesh2003 <someshthakur229@gmail.com>
2024-06-07 11:16:13 +0000

    For loop

---
+++ JS CHAI OR CODE/Loop_In_JS/01_For_Loop.js
@@ -0,0 +1,62 @@
+// let Table 
+// for (let i = 1; i <= 10; i++) {
+//         console.log(`table Of ${i}`,`\n`);
+//         for (let j = 1; j <= 10; j++) {
+//             console.log(i, `*`, j, `=`, i*j  );
+//             if(j==10)
+//                 {
+//                     console.log("\n");
+//                 }
+            
+            
+//         }    
+// }
+
+
+
+// let MyArr = ["janmesh", "somesh", "Thakur", "Ram", "pain", "Indore"]
+// for(let i = 0 ; i < MyArr.length ; i++ )
+// {
+//     console.log(MyArr[i]);
+// }
+
+/************************ Break And Continue ******************************/
+
+
+// for (let i = 1; i <= 20; i++) {
+//     if(i == 5)
+//     {
+//         console.log("5 is detected");
+//         break; /* break statement */
+//     }    
+//     console.log(i);
+// }
+
+
+// for (let i = 1; i <= 20; i++) {
+//     if(i == 5)
+//         {
+//             console.log("hi");
+//             continue
+//         }  
+//         console.log(i);   
+// }
+
+
+let v=2;
+for (let i = 1; i <= 20; i++) {
+    if(i%v == 1)
+    {
+        console.log(" ");
+        continue
+        
+    }
+    console.log(i);
+    
+}
+
+
+
+
+
+
